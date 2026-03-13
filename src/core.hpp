e690e1b39e1ced50a34f91e77c740ee9c6cebb1c
moonfloww <hugoifiorillo@gmail.com>
2026-03-13 14:03:19 +0100

    fix

--- src/core.hpp
+++ src/core.hpp
@@ -2449,9 +2449,10 @@ namespace Nott {
             if (!loss_descriptor_.has_value()) {
                 throw std::logic_error("Loss function has not been configured.");
             }
+            const torch::Tensor &aligned_target = target.device() == prediction.device() ? target : target.to(prediction.device());
             return std::visit(
                 [&](const auto &descriptor) {
-                    return Loss::Details::compute(descriptor, prediction, target, weight);
+                    return Loss::Details::compute(descriptor, prediction, aligned_target, weight);
                 }, *loss_descriptor_);
         }
 
